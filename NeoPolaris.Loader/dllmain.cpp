7023af70037832093ad41bbdb1cd775300ac1a5c
Aika <notaika@protonmail.com>
2022-02-18 22:37:07 -0500

    Un-hardcode path to Polaris DLL in loader

--- NeoPolaris.Loader/dllmain.cpp
+++ NeoPolaris.Loader/dllmain.cpp
@@ -2,17 +2,21 @@
 #include <metahost.h>
 #pragma comment(lib, "mscoree.lib")
 
-int main() {
-    ICLRMetaHost* pMetaHost = NULL;
-    ICLRRuntimeInfo* pRuntimeInfo = NULL;
-    ICLRRuntimeHost* pRuntimeHost = NULL;
+#include "util.h"
 
-    if (CLRCreateInstance(CLSID_CLRMetaHost, IID_ICLRMetaHost, reinterpret_cast<LPVOID*>(&pMetaHost)) == S_OK) {
-        if (pMetaHost->GetRuntime(L"v4.0.30319", IID_ICLRRuntimeInfo, reinterpret_cast<LPVOID*>(&pRuntimeInfo)) == S_OK) {
-            if (pRuntimeInfo->GetInterface(CLSID_CLRRuntimeHost, IID_ICLRRuntimeHost, reinterpret_cast<LPVOID*>(&pRuntimeHost)) == S_OK) {
-                if (pRuntimeHost->Start() == S_OK) {
-                    DWORD dwReturnValue;
-                    pRuntimeHost->ExecuteInDefaultAppDomain(L"C:\\Users\\Kaitlyn\\Documents\\GitHub\\NeoPolaris\\NeoPolaris\\bin\\Release\\Polaris.dll", L"NeoPolaris.Loader", L"HostedMain", L"", &dwReturnValue);
+int main() {
+    std::string modulePath = Util::GetModulePath();
+    std::string assemblyPath = Util::GetConcatPath(Util::GetDirPath(modulePath), "Polaris.dll");
+
+    ICLRMetaHost* metaHost;
+    ICLRRuntimeInfo* runtimeInfo;
+    ICLRRuntimeHost* runtimeHost;
+    if (CLRCreateInstance(CLSID_CLRMetaHost, IID_ICLRMetaHost, reinterpret_cast<LPVOID*>(&metaHost)) == S_OK) {
+        if (metaHost->GetRuntime(L"v4.0.30319", IID_ICLRRuntimeInfo, reinterpret_cast<LPVOID*>(&runtimeInfo)) == S_OK) {
+            if (runtimeInfo->GetInterface(CLSID_CLRRuntimeHost, IID_ICLRRuntimeHost, reinterpret_cast<LPVOID*>(&runtimeHost)) == S_OK) {
+                if (runtimeHost->Start() == S_OK) {
+                    DWORD returnValue;
+                    runtimeHost->ExecuteInDefaultAppDomain(Util::ToUtf16(assemblyPath, CP_UTF8).c_str(), L"NeoPolaris.Loader", L"HostedMain", L"", &returnValue);
                 }
             }
         }
